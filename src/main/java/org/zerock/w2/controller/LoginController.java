5435fb6b3740718abfedb1f5f0105b402a1c1106
손현지 <108772553+sonhyunji@users.noreply.github.com>
2023-03-03 13:14:52 +0900

    commit

--- src/main/java/org/zerock/w2/controller/LoginController.java
+++ src/main/java/org/zerock/w2/controller/LoginController.java
@@ -1,2 +1,52 @@
-package org.zerock.w2.controller;public class LoginController {
+package org.zerock.w2.controller;
+
+import lombok.extern.java.Log;
+import org.zerock.w2.dto.MemberDTO;
+import org.zerock.w2.service.MemberService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet("/login")
+@Log
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("login get..............");
+
+        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("login post.............");
+
+        String mid = req.getParameter("mid");
+        String mpw = req.getParameter("mpw");
+
+        String auto = req.getParameter("auto");
+
+        boolean rememberMe = auto != null && auto.equals("on");
+
+        if(rememberMe){
+            String uuid = UUID.randomUUID().toString();
+        }
+
+        try {
+            MemberDTO memberDTO = MemberService.INSTANCE.login(mid, mpw);
+            HttpSession session = req.getSession();
+            session.setAttribute("loginInfo", memberDTO);
+            resp.sendRedirect("/todo/list");
+        } catch (Exception e) {
+            resp.sendRedirect("/login?result=error");
+        }
+    }
+
 }
---
+++ src/main/java/org/zerock/w2/controller/TodoRemoveController.java
@@ -0,0 +1,30 @@
+package org.zerock.w2.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.zerock.w2.service.TodoService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "todoRemoveController", value = "/todo/remove")
+@Log4j2
+public class TodoRemoveController extends HttpServlet {
+    private TodoService todoService = TodoService.INSTANCE;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws SecurityException, IOException {
+        Long tno = Long.parseLong(req.getParameter("tno"));
+        log.info("tno: " + tno);
+
+        try {
+            todoService.remove(tno);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new SecurityException("read error");
+        }
+        resp.sendRedirect("/todo/list");
+    }
+}
